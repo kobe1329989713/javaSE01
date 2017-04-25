6e4ace57c97ada087233c1b2024325eac9c0a4b9
刘坤 <kun.liu@utsoft.cn>
2017-04-25 13:21:58 +0800

    解决bug

---
+++ src/com/Patterns/patterns01/IFruit.java
@@ -0,0 +1,22 @@
+package com.Patterns.patterns01;
+
+/**
+ * Created by ${kobe} on 2017/4/25.
+ */
+public interface IFruit {
+
+    /**
+     * 水果生长
+     */
+    void grow();
+
+    /**
+     * 收获
+     */
+    void harvest();
+
+    /**
+     * 种植。
+     */
+    void plant();
+}
