72a91b42f4855fef0b2e6de309c1df11095199b6
鲁亚强 <ya-qiang.lu@utsoft.cn>
2017-06-04 02:08:31 +0800

    x

--- src/com/javaSE03/day_27_reflect/pattern/itcast_01/GetTime.java
+++ src/com/javaSE03/day_27_reflect/pattern/itcast_01/GetTime.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+//模板模式
 public abstract class GetTime {
 	// 需求：请给我计算出一段代码的运行时间
 	public long getTime() {
@@ -33,12 +34,14 @@ public abstract class GetTime {
 		// }
 
 		// 再给我测试一个代码：集合操作的，多线程操作，常用API操作的等等...
-		code();
-
+		code(); // 把哪个需要测试 时间的代码，让子类自己去重写，这就是模板模式。？？？
+		// 因为它调用下面哪个抽象方法，
+		
 		long end = System.currentTimeMillis();
 
 		return end - start;
 	}
-
+	
+	// 而这个方法会被子类重写的，所以就可以测试任意代码的执行时间了，？？？,它就像 拼接起来样，只不过中间哪一陀是变化的。
 	public abstract void code();
 }
