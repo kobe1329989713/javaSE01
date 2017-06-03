a8a008d7bb13825972e04201d216af0b18dd0289
鲁亚强 <ya-qiang.lu@utsoft.cn>
2017-06-03 22:58:04 +0800

    reflect

---
+++ src/com/javaSE03/day_27_reflect/itcast_05/StudentDao.java
@@ -0,0 +1,7 @@
+package com.javaSE03.day_27_reflect.itcast_05;
+
+public interface StudentDao {
+	public abstract void login();
+
+	public abstract void regist();
+}
---
+++ src/com/javaSE03/day_27_reflect/itcast_06/UserDao.java
@@ -0,0 +1,14 @@
+package com.javaSE03.day_27_reflect.itcast_06;
+
+/*
+ * 用户操作接口
+ */
+public interface UserDao {
+	public abstract void add();
+
+	public abstract void delete();
+
+	public abstract void update();
+
+	public abstract void find();
+}
