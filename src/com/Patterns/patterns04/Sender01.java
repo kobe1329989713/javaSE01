5aea7ad10fb97ed3153caa69e0ce5523cda6a7fd
Administrator <1329989713@qq.com>
2017-06-16 11:02:44 +0800

    abstract factory

--- src/com/Patterns/patterns04/Sender01.java
+++ src/com/Patterns/patterns04/Sender01.java
@@ -58,29 +58,66 @@ class SendFactory {
 	}
 }
 
-// static factory Interface xxx
+// static factory Interface
+interface provider{
+	// 注：这里依赖的是 产品体系的抽象(父类与接口)
+	public Sender01 SmsSender();
+}
 
 // static factory
+class ProviderImpl implements provider{
+
+	@Override
+	public Sender01 SmsSender() {
+		return new mainSender01();
+	}
+}
+
+
+class ProviderImpl2 implements provider{
+
+	@Override
+	public Sender01 SmsSender() {
+		return new SmsSender02();
+	}
+}
 
 
 
 // test
 class Test01 {
 	public static void main(String[] args) {
+
+	    // static factory Test
+        // 多态的工厂
+        provider provider = new ProviderImpl();
+        // 多态的产品。左边是抽象，右边具体。
+        Sender01 is1 = provider.SmsSender();
+        is1.send();
+
+        // 2
+        provider provider2 = new ProviderImpl2();
+        Sender01 is2 = provider2.SmsSender();
+        is2.send();
+
+
+// 增加一个产品就要，增加一个工厂。
+
+
 		// 1、用的是factory，因为factory生产东东，用。
-		SendFactory factory = new SendFactory();
-		Sender01 is1 = factory.produce("mainSender01");
-		is1.send();
-
-		// 22
-		Sender01 is2 = factory.produce("SmsSender02");
-		is2.send();
-
-		System.out.println("===--// Test 静态工厂方法模式，多个工厂方法模式，就是不static 就好。--==");
-		Sender01 is3 = SendFactory.product01();
-		is3.send();
-		Sender01 is4 = SendFactory.product02();
-		is4.send();
+//		SendFactory factory = new SendFactory();
+//		Sender01 is1 = factory.produce("mainSender01");
+//		is1.send();
+//
+//		// 22
+//		Sender01 is2 = factory.produce("SmsSender02");
+//		is2.send();
+//
+//		System.out.println("===--// Test 静态工厂方法模式，多个工厂方法模式，就是不static 就好。--==");
+//		Sender01 is3 = SendFactory.product01();
+//		is3.send();
+//		Sender01 is4 = SendFactory.product02();
+//		is4.send();
 
 	}
 }
