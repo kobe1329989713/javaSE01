54c8092cc14c5cddd66eb6deecd8dcd60a6cb9c3
Administrator <1329989713@qq.com>
2017-06-21 10:15:18 +0800

    合并

---
+++ src/com/Patterns/patterns05/Mediator21.java
@@ -0,0 +1,86 @@
+package com.Patterns.patterns05;
+
+/**
+ * <br/>Author:Kobe
+ * <br/>Date:2017/6/19 23:39
+ * <br/>
+ *
+ *  中介者模式
+ *      中介者模式也是用来降低类类之间的耦合的
+ */
+    interface Mediator {
+        public void createMediator();
+        public void workAll();
+    }
+
+    class MyMediator implements Mediator {
+
+        private User user1;
+        private User user2;
+
+        public User getUser1() {
+            return user1;
+        }
+
+        public User getUser2() {
+            return user2;
+        }
+
+        @Override
+        public void createMediator() {
+            user1 = new User1(this);
+            user2 = new User2(this);
+        }
+
+        @Override
+        public void workAll() {
+            user1.work();
+            user2.work();
+        }
+    }
+
+    abstract class User {
+
+        private Mediator mediator;
+
+        public Mediator getMediator(){
+            return mediator;
+        }
+
+        public User(Mediator mediator) {
+            this.mediator = mediator;
+        }
+
+        public abstract void work();
+    }
+
+    class User1 extends User {
+
+        public User1(Mediator mediator){
+            super(mediator);
+        }
+
+        @Override
+        public void work() {
+            System.out.println("user1 exe!");
+        }
+    }
+
+    class User2 extends User {
+
+        public User2(Mediator mediator){
+            super(mediator);
+        }
+
+        @Override
+        public void work() {
+            System.out.println("user2 exe!");
+        }
+    }
+    public class Mediator21 {
+        public static void main(String[] args) {
+            Mediator mediator = new MyMediator();
+            mediator.createMediator();
+            mediator.workAll();
+        }
+    }
